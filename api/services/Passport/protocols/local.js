7a25d17a7419f3b73bdbe8b37c3460233e3f5f24
Maxime Bret <bret.maxime@gmail.com>
2015-10-02 00:06:36 +0200

    remove logs

--- api/services/Passport/protocols/local.js
+++ api/services/Passport/protocols/local.js
@@ -32,7 +32,6 @@ module.exports = function (req, email, password, next) {
         if (!user) {
             return next(null, false, { code: 400 });
         }
-        console.log(user);
 
         sails.models.passport.findOne({
             protocol : 'local',
@@ -51,7 +50,6 @@ module.exports = function (req, email, password, next) {
                         return next(null, false, { code: 400 });
                     }
                     else {
-                        console.log(user);
                         return next(null, user);
                     }
                 });
